1e70ad9c7be6d70264455b8f21735b269d18c9a5
Lucas Sales <lucas@yupistudios.com>
2021-11-26 15:41:05 -0300

    Melas app: CategoriesMealsScreen

---
+++ meals/lib/screens/categories_meals_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen({Key? key, required this.category})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text("Receitas por categoria ${category.id}"),
+      ),
+    );
+  }
+}
