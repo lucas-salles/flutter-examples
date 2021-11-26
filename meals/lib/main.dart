579cd89d57bd3526aa6bd824984c4f39105a60d4
Lucas Sales <lucas@yupistudios.com>
2021-11-26 12:08:18 -0300

    Meals app: Tema e Estilo

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -10,10 +10,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+      primarySwatch: Colors.pink,
+      fontFamily: 'Raleway',
+      canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+      textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            ),
+          ),
+    );
+
     return MaterialApp(
       title: 'Meals App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: Colors.amber,
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const CategoriesScreen(),
