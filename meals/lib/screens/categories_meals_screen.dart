8422e19e986a99b557ecd97439a86c10ec76b25c
Lucas Sales <lucas@yupistudios.com>
2021-11-29 13:58:05 -0300

    Meals app: ajustes

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/meal_item.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 import 'package:meals/models/meal.dart';
 
