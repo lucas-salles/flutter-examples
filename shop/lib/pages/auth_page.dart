e2e506f7ab1bf64de052ab0aa209d4f3530523c6
Lucas Sales <lucas@yupistudios.com>
2021-12-18 13:05:55 -0300

    Shop app: correção auth page

--- shop/lib/pages/auth_page.dart
+++ shop/lib/pages/auth_page.dart
@@ -8,6 +8,8 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isKeyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
+
     return Scaffold(
       body: Stack(
         children: [
@@ -24,34 +26,36 @@ class AuthPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                // Container(
-                //   margin: const EdgeInsets.only(bottom: 20),
-                //   padding: const EdgeInsets.symmetric(
-                //     horizontal: 70,
-                //     vertical: 10,
-                //   ),
-                //   transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                //   decoration: BoxDecoration(
-                //     borderRadius: BorderRadius.circular(20),
-                //     color: Colors.deepOrange.shade900,
-                //     boxShadow: const [
-                //       BoxShadow(
-                //         blurRadius: 8,
-                //         color: Colors.black26,
-                //         offset: Offset(0, 2),
-                //       ),
-                //     ],
-                //   ),
-                //   child: const Text(
-                //     'Minha Loja',
-                //     style: TextStyle(
-                //       fontSize: 45,
-                //       fontFamily: 'Anton',
-                //       color: Colors.white,
-                //     ),
-                //   ),
-                // ),
+              children: [
+                if (!isKeyboardVisible)
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 70,
+                      vertical: 10,
+                    ),
+                    transform: Matrix4.rotationZ(-8 * pi / 180)
+                      ..translate(-10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: const Text(
+                      'Minha Loja',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontFamily: 'Anton',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 AuthForm(),
               ],
             ),
