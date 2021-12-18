75035b09871efe0dc0a001654d79ed56e0961555
Lucas Sales <lucas@yupistudios.com>
2021-12-18 12:56:42 -0300

    Shop app: correção auth page

--- shop/lib/pages/auth_page.dart
+++ shop/lib/pages/auth_page.dart
@@ -24,35 +24,35 @@ class AuthPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 70,
-                    vertical: 10,
-                  ),
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: const Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Anton',
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const AuthForm(),
+              children: const [
+                // Container(
+                //   margin: const EdgeInsets.only(bottom: 20),
+                //   padding: const EdgeInsets.symmetric(
+                //     horizontal: 70,
+                //     vertical: 10,
+                //   ),
+                //   transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                //   decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.circular(20),
+                //     color: Colors.deepOrange.shade900,
+                //     boxShadow: const [
+                //       BoxShadow(
+                //         blurRadius: 8,
+                //         color: Colors.black26,
+                //         offset: Offset(0, 2),
+                //       ),
+                //     ],
+                //   ),
+                //   child: const Text(
+                //     'Minha Loja',
+                //     style: TextStyle(
+                //       fontSize: 45,
+                //       fontFamily: 'Anton',
+                //       color: Colors.white,
+                //     ),
+                //   ),
+                // ),
+                AuthForm(),
               ],
             ),
           ),
