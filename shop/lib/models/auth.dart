d9ec14ede6aabca43f94d6458da645e61bd6b154
Lucas Sales <lucas@yupistudios.com>
2021-12-20 11:14:09 -0300

    Shop app: correções

--- shop/lib/models/auth.dart
+++ shop/lib/models/auth.dart
@@ -27,7 +27,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
     final url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=YOUR_FIREBASE_KEY";
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=${const String.fromEnvironment("FIREBASESEKEY")}";
     final response = await http.post(
       Uri.parse(url),
       body: jsonEncode({
