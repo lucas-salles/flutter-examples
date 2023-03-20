dc49265e66ad2a56f9150b11a99eed5c72ac05e4
Lucas Sales <lucassales398@gmail.com>
2023-03-20 13:05:29 -0300

    For Dev: move api details from domain class to data class

--- for_dev/lib/domain/usecases/authentication.dart
+++ for_dev/lib/domain/usecases/authentication.dart
@@ -9,6 +9,4 @@ class AuthenticationParams {
   final String secret;
 
   AuthenticationParams({required this.email, required this.secret});
-
-  Map toJson() => {'email': email, 'password': secret};
 }
