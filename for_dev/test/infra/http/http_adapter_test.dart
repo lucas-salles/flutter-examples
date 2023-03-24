2a05c9545d032b3908f992a3bfc86b09d7c2a348
Lucas Sales <lucassales398@gmail.com>
2023-03-24 16:28:24 -0300

    For Dev: ensure HttpAdapter returns empty map if post returns 204

--- for_dev/test/infra/http/http_adapter_test.dart
+++ for_dev/test/infra/http/http_adapter_test.dart
@@ -98,5 +98,13 @@ void main() {
 
       expect(response, {});
     });
+
+    test('Should return empty map if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, {});
+    });
   });
 }
