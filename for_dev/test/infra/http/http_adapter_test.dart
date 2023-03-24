1a2473ea011b6be3e4d904e2f4b8cb42d0a3d0ac
Lucas Sales <lucassales398@gmail.com>
2023-03-24 16:36:25 -0300

    For Dev: move HtppAdapter to its own file

--- for_dev/test/infra/http/http_adapter_test.dart
+++ for_dev/test/infra/http/http_adapter_test.dart
@@ -1,42 +1,14 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'package:for_dev/data/http/http.dart';
+import 'package:for_dev/infra/http/http.dart';
 
 // Annotation which generates the http_adapter_test.mocks.dart library and the MockClient class.
 @GenerateNiceMocks([MockSpec<Client>()])
 import './http_adapter_test.mocks.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  @override
-  Future<Map> request(
-      {required String url, required String method, Map? body}) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonBody,
-    );
-    if (response.statusCode == 200) {
-      return response.body.isNotEmpty ? jsonDecode(response.body) : {};
-    } else {
-      return {};
-    }
-  }
-}
 
 void main() {
   // sut = system under test
