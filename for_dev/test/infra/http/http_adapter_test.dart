6fd890fb44c9d2b91dc2db0ae6442f2a4a4971a0
Lucas Sales <lucassales398@gmail.com>
2023-03-24 16:46:11 -0300

    For Dev: ensure HttpAdapter throws BadRequestError on 400

--- for_dev/test/infra/http/http_adapter_test.dart
+++ for_dev/test/infra/http/http_adapter_test.dart
@@ -93,17 +93,17 @@ void main() {
       expect(response, {});
     });
 
-    test('Should return BadRequestError if post returns 400', () async {
-      mockResponse(400);
+    test('Should return BadRequestError if post with empty body returns 400',
+        () async {
+      mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should return BadRequestError if post with empty body returns 400',
-        () async {
-      mockResponse(400, body: '');
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
