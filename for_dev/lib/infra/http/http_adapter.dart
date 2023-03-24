ef8ce834ea3192cf3424d2da45dac98abe7efd98
Lucas Sales <lucassales398@gmail.com>
2023-03-24 16:48:38 -0300

    For Dev: ensure HttpAdapter throws ServerError on 500

--- for_dev/lib/infra/http/http_adapter.dart
+++ for_dev/lib/infra/http/http_adapter.dart
@@ -30,8 +30,10 @@ class HttpAdapter implements HttpClient {
       return response.body.isNotEmpty ? jsonDecode(response.body) : {};
     } else if (response.statusCode == 204) {
       return {};
-    } else {
+    } else if (response.statusCode == 400) {
       throw HttpError.badRequest;
+    } else {
+      throw HttpError.serverError;
     }
   }
 }
