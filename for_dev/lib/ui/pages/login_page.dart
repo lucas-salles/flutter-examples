0aa0767493c654550caefcac74dd946eaa0a8c88
Lucas Sales <lucassales398@gmail.com>
2023-03-27 12:16:23 -0300

    For Dev: ensure LoginPage loads with correct initial state

--- for_dev/lib/ui/pages/login_page.dart
+++ for_dev/lib/ui/pages/login_page.dart
@@ -41,7 +41,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: null,
                       child: Text(
                         'Entrar'.toUpperCase(),
                       ),
