7c9ef1bc0a49c4317a8c79b5ee5de7262f1acc3f
Lucas Sales <lucassales398@gmail.com>
2023-03-27 11:22:35 -0300

    For Dev: add LoginPage layout

---
+++ for_dev/lib/ui/components/app.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../pages/pages.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+    final ThemeData theme = ThemeData();
+
+    return MaterialApp(
+      title: '4Dev',
+      theme: theme.copyWith(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        colorScheme: theme.colorScheme.copyWith(
+          primary: primaryColor,
+          secondary: primaryColor,
+          background: Colors.white,
+        ),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorDark,
+          ),
+          titleMedium: TextStyle(
+            color: primaryColor,
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          iconColor: primaryColorLight,
+          labelStyle: TextStyle(color: primaryColorLight),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: primaryColorLight,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: primaryColor,
+            ),
+          ),
+          alignLabelWithHint: true,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ for_dev/lib/ui/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const LoginHeader(),
+            Text(
+              'Login'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Entrar'.toUpperCase(),
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Criar Conta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
