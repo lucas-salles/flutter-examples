f54903d3323b66f81d253fccb54cc2884d02374d
Lucas Sales <lucassales398@gmail.com>
2023-03-28 14:45:03 -0300

    For Dev: move ErrorMessage to its own file

--- for_dev/lib/ui/pages/login/login_page.dart
+++ for_dev/lib/ui/pages/login/login_page.dart
@@ -33,15 +33,7 @@ class _LoginPageState extends State<LoginPage> {
           });
 
           widget.presenter?.mainErrorStream.listen((error) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: Colors.red[900],
-                content: Text(
-                  error,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            );
+            showErrorMessage(context, error);
           });
 
           return SingleChildScrollView(
