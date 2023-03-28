fe77f15eac334f05b33c6a40fd96a3e53141608a
Lucas Sales <lucassales398@gmail.com>
2023-03-28 12:09:28 -0300

    For Dev: enusre LoginPage disables button if form is invalid

--- for_dev/test/ui/pages/login_page_test.dart
+++ for_dev/test/ui/pages/login_page_test.dart
@@ -143,4 +143,15 @@ void main() {
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, isNotNull);
   });
+
+  testWidgets('Should disables button if form is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isFormValidController.add(false);
+    await tester.pump();
+
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+  });
 }
