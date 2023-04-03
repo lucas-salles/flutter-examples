c253a18676c797a4c3bee1ad64e6898b43e4a1fc
Lucas Sales <lucassales398@gmail.com>
2023-04-03 15:32:51 -0300

    For Dev: move StreamLoginPresenter to its own file

---
+++ for_dev/lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import '../protocols/protocols.dart';
+
+class LoginState {
+  String? emailError;
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  final _controller = StreamController<LoginState>.broadcast();
+
+  final _state = LoginState();
+
+  StreamLoginPresenter({required this.validation});
+
+  Stream<String?> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
--- for_dev/test/presentation/presenters/stream_login_presenter_test.dart
+++ for_dev/test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,37 +1,14 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'package:for_dev/presentation/protocols/validation.dart';
+import 'package:for_dev/presentation/presenters/presenters.dart';
+import 'package:for_dev/presentation/protocols/protocols.dart';
 
 // Annotation which generates the stream_login_presenter_test.mocks.dart library and the MockValidation class.
 @GenerateNiceMocks([MockSpec<Validation>()])
 import './stream_login_presenter_test.mocks.dart';
-
-class LoginState {
-  String? emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-
-  final _controller = StreamController<LoginState>.broadcast();
-
-  final _state = LoginState();
-
-  StreamLoginPresenter({required this.validation});
-
-  Stream<String?> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
 
 void main() {
   late StreamLoginPresenter sut;
