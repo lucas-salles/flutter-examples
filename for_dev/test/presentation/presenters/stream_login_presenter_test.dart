6fdd165c6fc3af714f341d01b1660929108290db
Lucas Sales <lucassales398@gmail.com>
2023-04-03 16:26:26 -0300

    For Dev: ensure StreamLoginPresenter emits emailError as null if email is valid

--- for_dev/test/presentation/presenters/stream_login_presenter_test.dart
+++ for_dev/test/presentation/presenters/stream_login_presenter_test.dart
@@ -46,4 +46,13 @@ void main() {
     sut.validateEmail(email);
     sut.validateEmail(email);
   });
+
+  test('Should emit null if validation succeeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isValidFormStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
 }
