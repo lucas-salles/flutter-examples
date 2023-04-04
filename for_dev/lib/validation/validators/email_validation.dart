e0abec484c0ba8afc3bbaed8ef862419edd29763
Lucas Sales <lucassales398@gmail.com>
2023-04-04 16:03:34 -0300

    For Dev: move EmailValidation to its own file

---
+++ for_dev/lib/validation/validators/email_validation.dart
@@ -0,0 +1,15 @@
+import '../protocols/protocols.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  const EmailValidation(this.field);
+
+  @override
+  String? validate(String? value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
+    return isValid ? null : 'Campo inválido';
+  }
+}
--- for_dev/test/validation/validators/email_validation_test.dart
+++ for_dev/test/validation/validators/email_validation_test.dart
@@ -1,20 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:for_dev/validation/protocols/protocols.dart';
-
-class EmailValidation implements FieldValidation {
-  final String field;
-
-  const EmailValidation(this.field);
-
-  @override
-  String? validate(String? value) {
-    final regex = RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
-    return isValid ? null : 'Campo inválido';
-  }
-}
+import 'package:for_dev/validation/validators/validators.dart';
 
 void main() {
   late EmailValidation sut;
