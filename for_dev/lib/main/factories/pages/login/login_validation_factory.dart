6e0c801228f82194d7db439b0f2e54a044c718a7
Lucas Sales <lucassales398@gmail.com>
2023-04-11 10:45:30 -0300

    For Dev: add ValidationBuilder

--- for_dev/lib/main/factories/pages/login/login_validation_factory.dart
+++ for_dev/lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import '../../../builders/builders.dart';
 import '../../../../presentation/protocols/protocols.dart';
 import '../../../../validation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
@@ -7,9 +8,8 @@ Validation makeLoginValidation() {
 }
 
 List<FieldValidation> makeLoginValidations() {
-  return const [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+  return [
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
