5dfde8d0a2f2901a30749fe18cc75973b39a9e29
Lucas Sales <lucassales398@gmail.com>
2023-04-11 14:32:34 -0300

    For Dev: ensure LocalSaveCurrentAccount throws UnexpectedError if MockSaveSecureCacheStorage throws

--- for_dev/test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ for_dev/test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
@@ -17,7 +18,12 @@ class LocalSaveCurrentAccount implements SaveCurrentAccount {
 
   @override
   Future<void> save(AccountEntity account) async {
-    await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    try {
+      await saveSecureCacheStorage.saveSecure(
+          key: 'token', value: account.token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -26,7 +32,7 @@ abstract class SaveSecureCacheStorage {
 }
 
 void main() {
-  test('Should call SaveCacheStorage with correct values', () async {
+  test('Should call SaveSecureCacheStorage with correct values', () async {
     final saveSecureCacheStorage = MockSaveSecureCacheStorage();
     final sut =
         LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
@@ -38,5 +44,20 @@ void main() {
       key: 'token',
       value: account.token,
     ));
+  });
+
+  test('Should throw UnexpectedError if SaveSecureCacheStorage throws',
+      () async {
+    final saveSecureCacheStorage = MockSaveSecureCacheStorage();
+    final sut =
+        LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    final account = AccountEntity(faker.guid.guid());
+    when(saveSecureCacheStorage.saveSecure(
+            key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+
+    final future = sut.save(account);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
