90547e90649bca889b7308862711b2bdc5b2451b
Lucas Sales <lucassales398@gmail.com>
2023-04-11 15:18:29 -0300

    For Dev: move LocalStorageAdapter to its own file

---
+++ for_dev/lib/infra/cache/local_storage_adapter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../data/cache/cache.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+
+  const LocalStorageAdapter({required this.secureStorage});
+
+  @override
+  Future<void> saveSecure({required String key, required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
--- for_dev/test/infra/cache/local_storage_adapter_test.dart
+++ for_dev/test/infra/cache/local_storage_adapter_test.dart
@@ -4,22 +4,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'package:for_dev/data/cache/cache.dart';
+import 'package:for_dev/infra/cache/cache.dart';
 
 // Annotation which generates the local_storage_adapter_test.mocks.dart library and the MockClient class.
 @GenerateNiceMocks([MockSpec<FlutterSecureStorage>()])
 import './local_storage_adapter_test.mocks.dart';
-
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-
-  const LocalStorageAdapter({required this.secureStorage});
-
-  @override
-  Future<void> saveSecure({required String key, required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 void main() {
   late MockFlutterSecureStorage secureStorage;
