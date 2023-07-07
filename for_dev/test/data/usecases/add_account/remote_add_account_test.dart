faea05db71286eacdc1f43e12a1232b3f3604c86
Lucas Sales <lucassales398@gmail.com>
2023-07-07 16:23:23 -0300

    ensure RemoteAddAccount calls HttpClient with correct values

---
+++ for_dev/test/data/usecases/add_account/remote_add_account_test.dart
@@ -0,0 +1,47 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:for_dev/domain/usecases/usecases.dart';
+
+import 'package:for_dev/data/http/http.dart';
+import 'package:for_dev/data/usecases/usecases.dart';
+
+// Annotation which generates the remote_add_account_test.mocks.dart library and the MockHttpClient class.
+@GenerateNiceMocks([MockSpec<HttpClient>()])
+import './remote_add_account_test.mocks.dart';
+
+void main() {
+  late RemoteAddAccount sut;
+  late MockHttpClient httpClient;
+  late String url;
+  late AddAccountParams params;
+
+  setUp(() {
+    httpClient = MockHttpClient();
+    url = faker.internet.httpUrl();
+    sut = RemoteAddAccount(httpClient: httpClient, url: url);
+    params = AddAccountParams(
+      name: faker.person.name(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      passwordConfirmation: faker.internet.password(),
+    );
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.add(params);
+
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {
+        'name': params.name,
+        'email': params.email,
+        'password': params.password,
+        'passwordConfirmation': params.passwordConfirmation,
+      },
+    ));
+  });
+}
