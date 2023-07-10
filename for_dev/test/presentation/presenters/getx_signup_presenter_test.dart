147a3b6336c0eb3bf13c9f35bc4774dba3ab3c65
Lucas Sales <lucassales398@gmail.com>
2023-07-10 17:17:37 -0300

    ensure GetxSignUpPresenter emits error if AddAccount fails

--- for_dev/test/presentation/presenters/getx_signup_presenter_test.dart
+++ for_dev/test/presentation/presenters/getx_signup_presenter_test.dart
@@ -44,6 +44,10 @@ void main() {
     mockAddAccountCall().thenAnswer((_) async => AccountEntity(token));
   }
 
+  void mockAuthenticationError(DomainError error) {
+    mockAddAccountCall().thenThrow(error);
+  }
+
   PostExpectation mockSaveCurrentAccountCall() =>
       when(saveCurrentAccount.save(any));
 
@@ -295,6 +299,34 @@ void main() {
     sut.validatePasswordConfirmation(passwordConfirmation);
 
     expectLater(sut.isLoadingStream, emits(true));
+
+    await sut.signUp();
+  });
+
+  test('Should emit correct events on EmailInUseError', () async {
+    mockAuthenticationError(DomainError.emailInUse);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        ?.listen(expectAsync1((error) => expect(error, UIError.emailInUse)));
+
+    await sut.signUp();
+  });
+
+  test('Should emit correct events on UnexpectedError', () async {
+    mockAuthenticationError(DomainError.unexpected);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream
+        ?.listen(expectAsync1((error) => expect(error, UIError.unexpected)));
 
     await sut.signUp();
   });
