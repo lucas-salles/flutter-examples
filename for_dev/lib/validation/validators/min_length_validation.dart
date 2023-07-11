c74c4c75a55f5d9516ffce82984c6367f786cac6
Lucas Sales <lucassales398@gmail.com>
2023-07-11 11:16:37 -0300

    change Login composition to use MinLengthValidation on password

--- for_dev/lib/validation/validators/min_length_validation.dart
+++ for_dev/lib/validation/validators/min_length_validation.dart
@@ -1,13 +1,18 @@
+import 'package:equatable/equatable.dart';
+
 import '../../presentation/protocols/protocols.dart';
 
 import '../protocols/protocols.dart';
 
-class MinLengthValidation implements FieldValidation {
+class MinLengthValidation extends Equatable implements FieldValidation {
   @override
   final String field;
   final int size;
 
-  MinLengthValidation({required this.field, required this.size});
+  const MinLengthValidation({required this.field, required this.size});
+
+  @override
+  List get props => [field, size];
 
   @override
   ValidationError? validate(String? value) {
--- for_dev/test/main/factories/pages/login/login_validation_factory_test.dart
+++ for_dev/test/main/factories/pages/login/login_validation_factory_test.dart
@@ -11,6 +11,7 @@ void main() {
       RequiredFieldValidation('email'),
       EmailValidation('email'),
       RequiredFieldValidation('password'),
+      MinLengthValidation(field: 'password', size: 3),
     ]);
   });
 }
