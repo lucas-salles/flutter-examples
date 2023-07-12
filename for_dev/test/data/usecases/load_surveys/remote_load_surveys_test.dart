2b2e305664f774c98e034e90294f84a450302625
Lucas Sales <lucassales398@gmail.com>
2023-07-12 15:42:13 -0300

    move RemoteLoadSurveys to its own file

--- for_dev/test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ for_dev/test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -8,34 +8,11 @@ import 'package:for_dev/domain/entities/entities.dart';
 import 'package:for_dev/domain/helpers/helpers.dart';
 
 import 'package:for_dev/data/http/http.dart';
-import 'package:for_dev/data/models/models.dart';
+import 'package:for_dev/data/usecases/usecases.dart';
 
 // Annotation which generates the remote_load_surveys_test.mocks.dart library and the MockHttpClient class.
 @GenerateNiceMocks([MockSpec<HttpClient<List<Map>>>()])
 import './remote_load_surveys_test.mocks.dart';
-
-class RemoteLoadSurveys {
-  final String url;
-  final HttpClient<List<Map>> httpClient;
-
-  const RemoteLoadSurveys({
-    required this.url,
-    required this.httpClient,
-  });
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final response = await httpClient.request(url: url, method: 'get');
-      return response
-          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } on HttpError catch (error) {
-      throw error == HttpError.forbidden
-          ? DomainError.accessDenied
-          : DomainError.unexpected;
-    }
-  }
-}
 
 void main() {
   late String url;
