d3fb41442a1917f0d5bebaa15d9809e883e98b6e
Lucas Sales <lucassales398@gmail.com>
2023-07-13 16:17:54 -0300

    move GetxSurveysPresenter to its own file

--- for_dev/test/presentation/presenters/getx_surveys_presenter_test.dart
+++ for_dev/test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -2,8 +2,6 @@ import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:for_dev/ui/helpers/helpers.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
@@ -12,40 +10,11 @@ import 'package:for_dev/domain/usecases/usecases.dart';
 
 import 'package:for_dev/ui/pages/pages.dart';
 
+import 'package:for_dev/presentation/presenters/presenters.dart';
+
 // Annotation which generates the getx_surveys_presenter_test.mocks.dart library and the MockLoadSurveys class.
 @GenerateNiceMocks([MockSpec<LoadSurveys>()])
 import './getx_surveys_presenter_test.mocks.dart';
-
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>(List<SurveyViewModel>.empty());
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM  yyyy').format(survey.dateTime),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
 
 void main() {
   late MockLoadSurveys loadSurveys;
