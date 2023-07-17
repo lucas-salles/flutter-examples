7452025012f4af6edcb6c8d8e9608ebaa54817f5
Lucas Sales <lucassales398@gmail.com>
2023-07-17 11:45:07 -0300

    make LocalLoadSurveys implements LoadSurveys

--- for_dev/lib/data/usecases/load_surveys/local_load_surveys.dart
+++ for_dev/lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -1,13 +1,15 @@
 import '../../../domain/entities/entities.dart';
 import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
 import '../../cache/cache.dart';
 import '../../models/models.dart';
 
-class LocalLoadSurveys {
+class LocalLoadSurveys implements LoadSurveys {
   final FetchCacheStorage fetchCacheStorage;
 
   LocalLoadSurveys({required this.fetchCacheStorage});
 
+  @override
   Future<List<SurveyEntity>> load() async {
     try {
       final data = await fetchCacheStorage.fetch('surveys');
