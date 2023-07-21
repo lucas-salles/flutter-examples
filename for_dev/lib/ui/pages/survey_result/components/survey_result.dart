8fd9c8282e1c1937029cdb18fab496b356a1c549
Lucas Sales <lucassales398@gmail.com>
2023-07-21 11:37:49 -0300

    move SurveyResult Component to its own file

---
+++ for_dev/lib/ui/pages/survey_result/components/survey_result.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class SurveyResult extends StatelessWidget {
+  const SurveyResult({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return Container(
+            padding: const EdgeInsets.only(
+              top: 40,
+              bottom: 20,
+              left: 20,
+              right: 20,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).disabledColor.withAlpha(90),
+            ),
+            child: const Text('Qual é o seu framework web favorito?'),
+          );
+        }
+        return Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.background,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.network(
+                      'https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK_400x400.png',
+                      width: 40,
+                    ),
+                    const Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'React',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '100%',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Theme.of(context).highlightColor,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Divider(height: 1)
+          ],
+        );
+      },
+      itemCount: 4,
+    );
+  }
+}
--- for_dev/lib/ui/pages/survey_result/survey_result_page.dart
+++ for_dev/lib/ui/pages/survey_result/survey_result_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
+import 'components/components.dart';
 import 'survey_result_presenter.dart';
 
 class SurveyResultPage extends StatelessWidget {
@@ -37,73 +38,7 @@ class SurveyResultPage extends StatelessWidget {
               }
 
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return Container(
-                        padding: const EdgeInsets.only(
-                          top: 40,
-                          bottom: 20,
-                          left: 20,
-                          right: 20,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).disabledColor.withAlpha(90),
-                        ),
-                        child:
-                            const Text('Qual é o seu framework web favorito?'),
-                      );
-                    }
-                    return Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.background,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Image.network(
-                                  'https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK_400x400.png',
-                                  width: 40,
-                                ),
-                                const Expanded(
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 10),
-                                    child: Text(
-                                      'React',
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                  ),
-                                ),
-                                Text(
-                                  '100%',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context).primaryColorDark,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  child: Icon(
-                                    Icons.check_circle,
-                                    color: Theme.of(context).highlightColor,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        const Divider(height: 1)
-                      ],
-                    );
-                  },
-                  itemCount: 4,
-                );
+                return const SurveyResult();
               }
 
               return Container();
