f6bde26b52b54d9b569660dfd162ca0518fb5843
Lucas Sales <lucassales398@gmail.com>
2023-07-23 12:39:05 -0300

    add LocalLoadSurveyResult loadBySurvey with tests

---
+++ for_dev/test/data/usecases/load_survey_result/local_load_survey_result_test.dart
@@ -0,0 +1,139 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:for_dev/domain/entities/entities.dart';
+import 'package:for_dev/domain/helpers/helpers.dart';
+import 'package:for_dev/data/cache/cache.dart';
+import 'package:for_dev/data/usecases/usecases.dart';
+
+// Annotation which generates the local_load_survey_result_test.mocks.dart library and the MockCacheStorage class.
+@GenerateNiceMocks([MockSpec<CacheStorage>()])
+import './local_load_survey_result_test.mocks.dart';
+
+void main() {
+  group('loadBySurvey', () {
+    late MockCacheStorage cacheStorage;
+    late LocalLoadSurveyResult sut;
+    late Map? data;
+    late String surveyId;
+
+    Map mockValidData() => {
+          'surveyId': faker.guid.guid(),
+          'question': faker.lorem.sentence(),
+          'answers': [
+            {
+              'image': faker.internet.httpUrl(),
+              'answer': faker.lorem.sentence(),
+              'isCurrentAnswer': 'true',
+              'percent': '40',
+            },
+            {
+              'answer': faker.lorem.sentence(),
+              'isCurrentAnswer': 'false',
+              'percent': '60',
+            },
+          ],
+        };
+
+    PostExpectation mockFetchCall() => when(cacheStorage.fetch(any));
+
+    void mockFetch(Map? json) {
+      data = json;
+      mockFetchCall().thenAnswer((_) async => data);
+    }
+
+    void mockFetchError() => mockFetchCall().thenThrow(Exception());
+
+    setUp(() {
+      surveyId = faker.guid.guid();
+      cacheStorage = MockCacheStorage();
+      sut = LocalLoadSurveyResult(cacheStorage: cacheStorage);
+      mockFetch(mockValidData());
+    });
+
+    test('Should call CacheStorage with correct key', () async {
+      await sut.loadBySurvey(surveyId: surveyId);
+
+      verify(cacheStorage.fetch('survey_result/$surveyId')).called(1);
+    });
+
+    test('Should return surveyResult on success', () async {
+      final surveyResult = await sut.loadBySurvey(surveyId: surveyId);
+
+      expect(
+          surveyResult,
+          SurveyResultEntity(
+            surveyId: data!['surveyId'],
+            question: data!['question'],
+            answers: [
+              SurveyAnswerEntity(
+                image: data!['answers'][0]['image'],
+                answer: data!['answers'][0]['answer'],
+                isCurrentAnswer: true,
+                percent: 40,
+              ),
+              SurveyAnswerEntity(
+                answer: data!['answers'][1]['answer'],
+                isCurrentAnswer: false,
+                percent: 60,
+              ),
+            ],
+          ));
+    });
+
+    test('Should throw UnexpectedError if cache is empty', () async {
+      mockFetch({});
+
+      final future = sut.loadBySurvey(surveyId: surveyId);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache is null', () async {
+      mockFetch(null);
+
+      final future = sut.loadBySurvey(surveyId: surveyId);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache is invalid', () async {
+      mockFetch({
+        'surveyId': faker.guid.guid(),
+        'question': faker.lorem.sentence(),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.lorem.sentence(),
+            'isCurrentAnswer': 'invalid bool',
+            'percent': 'invalid int',
+          },
+        ],
+      });
+
+      final future = sut.loadBySurvey(surveyId: surveyId);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache is incomplete', () async {
+      mockFetch({
+        'surveyId': faker.guid.guid(),
+      });
+
+      final future = sut.loadBySurvey(surveyId: surveyId);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache throws', () async {
+      mockFetchError();
+
+      final future = sut.loadBySurvey(surveyId: surveyId);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+  });
+}
