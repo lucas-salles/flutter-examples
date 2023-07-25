f2c7b0cad091542680f2fb3cd42bcb5521dbddeb
Lucas Sales <lucassales398@gmail.com>
2023-07-25 15:45:52 -0300

    allows navigation stream to send the same event twice

--- for_dev/test/presentation/presenters/getx_surveys_presenter_test.dart
+++ for_dev/test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -102,9 +102,10 @@ void main() {
   });
 
   test('Should go to SurveyResultPage on survey click', () async {
-    sut.navigateToStream.listen(
-        expectAsync1((page) => expect(page, '/survey_result/any_route')));
+    expectLater(sut.navigateToStream,
+        emitsInOrder(['/survey_result/any_route', '/survey_result/any_route']));
 
+    sut.goToSurveyResult('any_route');
     sut.goToSurveyResult('any_route');
   });
 }
